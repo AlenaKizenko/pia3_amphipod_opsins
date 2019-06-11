cdacc3c4d1f3d40b5bbb1b0340439f7276728a1d
AlenaKizenko <43323627+AlenaKizenko@users.noreply.github.com>
2019-06-11 22:06:39 +0300

    adding comments

--- scripts/sra_download.sh
+++ scripts/sra_download.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-lst=("SRR3532634" "SRR3532641" "SRR3532642")
+lst=("SRR3532634" "SRR3532641" "SRR3532642") #listing SRA names of reads
 for i in ${!lst[@]};
-do
+do #downloading data on server
 	/media/secondary/apps/sratoolkit.2.9.2-ubuntu64/bin/fastq-dump --outdir /media/tertiary/Alena_Kizenko/hyalella_azteca_reads --gzip --defline-seq '@$sn[_$rn]/$ri' --readids --split-files ${lst[$i]}
 done
