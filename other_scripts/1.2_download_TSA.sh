dbdb19453c863bfbf3d772541e711fd4fd578ab1
AlenaKizenko <alyona.kizenko@gmail.com>
2020-11-22 17:38:09 +0100

    other scripts editing

--- other_scripts/1.2_download_TSA.sh
+++ other_scripts/1.2_download_TSA.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-  cat amphipoda_TSA.tsv| while IFS=$'\t' read -r col1 col2
-	mkdir /media/quartemary/TSA/${col2}_${col1}
+  cat 1.2_amphipoda_TSA.csv | while IFS=$'\t' read -r col1 col2
+	mkdir ${PATH_TO_DIR}/${col2}_${col1}
 	echo "$col2 $col1 started downloading"
-    	/media/secondary/apps/sratoolkit.2.9.2-ubuntu64/bin/fastq-dump --outdir /media/quartemary/TSA/${col2}_${col1} -F --fasta $col1
+    	fastq-dump --outdir ${PATH_TO_DIR}/${col2}_${col1} -F --fasta $col1
 	echo "$col2 $col1 ready"
   done
 
