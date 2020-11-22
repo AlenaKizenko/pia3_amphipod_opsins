eefca3acfa0396053d156029b429e14cb0eb8b17
AlenaKizenko <43323627+AlenaKizenko@users.noreply.github.com>
2020-11-22 17:32:54 +0100

    Update 1.1_download_SRA.sh

--- other_scripts/1.1_download_SRA.sh
+++ other_scripts/1.1_download_SRA.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-  cat /media/tertiary/Alena_Kizenko/SRA_gammaridae.csv| while IFS=, read -r col1 col2
+  cat data/SRA_gammaridae.csv| while IFS=, read -r col1 col2
   do
-	if [ -d "/media/quartemary/rnaseq_reads/${col2}" ]
+	if [ -d "${PATH_TO_DIR}/${col2}" ] # if directory already exists
 	then
 		echo "Reads for $col2 are already downloaded"
 	else
-		mkdir /media/quartemary/rnaseq_reads/${col2}
+		mkdir ${PATH_TO_DIR}/${col2}
 		echo "$col2 directory has been created"
 		echo "$col1 reads start downloading"
-    		/media/secondary/apps/sratoolkit.2.9.2-ubuntu64/bin/fastq-dump --outdir /media/quartemary/rnaseq_reads/$col2 --gzip --defline-seq '@$sn[_$rn]/$ri' --readids --split-3 $col1
+    		fastq-dump --outdir ${PATH_TO_DIR}/$col2 --gzip --defline-seq '@$sn[_$rn]/$ri' --readids --split-3 $col1 # download reads via fastq-dump
 		echo "$col1 reads have been downloaded in $col2 directory"
 	fi
   done
