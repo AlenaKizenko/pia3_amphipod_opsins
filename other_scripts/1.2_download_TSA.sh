d205ad599b38aacb8d1f595239da805c1523be67
Polina Drozdova <drozdovapb@gmail.com>
2020-11-26 17:01:57 +0800

    corrected a tiny file format error

--- other_scripts/1.2_download_TSA.sh
+++ other_scripts/1.2_download_TSA.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-  cat 1.2_amphipoda_TSA.csv | while IFS=$'\t' read -r col1 col2
+  cat 1.2_amphipoda_TSA.tsv | while IFS=$'\t' read -r col1 col2
 	mkdir ${PATH_TO_DIR}/${col2}_${col1}
 	echo "$col2 $col1 started downloading"
     	fastq-dump --outdir ${PATH_TO_DIR}/${col2}_${col1} -F --fasta $col1
