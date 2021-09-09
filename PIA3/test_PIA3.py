f685e833ae58fae5c00f6f81c16bf1f08141eaee
AlenaKizenko <43323627+AlenaKizenko@users.noreply.github.com>
2021-09-09 14:12:01 +0200

    Update test_PIA3.py

--- PIA3/test_PIA3.py
+++ PIA3/test_PIA3.py
@@ -6,12 +6,12 @@ from Bio import SeqIO
 class TestStringMethods(unittest.TestCase):
 
   def test_existence(self):
-      self.assertTrue(os.path.isfile('./test_out/test_Hyalella_azteca/test_Hyalella_azteca_opsins_class.fasta'))
+      self.assertTrue(os.path.isfile('./test_out/test_Hyalella_azteca/test_Hyalella_azteca_PIA3_aa.fasta'))
 
   def test_file(self):
       seq_num = 0
       opsins_type = []
-      for seq_record in SeqIO.parse('./test_out/test_Hyalella_azteca/test_Hyalella_azteca_opsins_class.fasta', "fasta"):
+      for seq_record in SeqIO.parse('./test_out/test_Hyalella_azteca/test_Hyalella_azteca_PIA3_aa.fasta', "fasta"):
           opsins_type.append(seq_record.id[0:3])
           seq_num += 1
       print(opsins_type)
